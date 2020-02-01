399894678551998dcd8cc2bf05b338915b2aa82e
Michael Liu <michaelangeliu+git@gmail.com>
2020-02-01 14:41:34 -0600

    Update resume styles
    - Add hidden class to hide sections
    - Change font sizes and spacing to create better white space management
    and introduce hierarchy to the page

--- michaelangeliu/src/pages/Resume.js
+++ michaelangeliu/src/pages/Resume.js
@@ -12,16 +12,20 @@ function Resume() {
           <summary>Detail-oriented Software Developer with a 5+ years experience designing and implementing holistic solutions effectively and efficiently. Developed web applications for multi-national brands. Prototyped new features for an online CRM tool. Skilled in creating new documentation to improve long-term code maintainability, development process, and product stewardship.</summary>
         </section>
         ~
-        <section className="work-experience">
-          <h3>Work Experience</h3>
+        <section className="work">
+          <h2>Work Experience</h2>
           <article className="experience">
             <p>
               <span className="organization">Sherpa CRM</span>
               <span className="location">Saint Louis, MO</span>
             </p>
             <p>
+              <span className="title">Senior Software Developer</span>
+              <span className="time-period">Sep 2019 – Present</span>
+            </p>
+            <p>
               <span className="title">Software Developer</span>
-              <span className="time-period">Aug 2017 – Present</span>
+              <span className="time-period">Aug 2017 – Aug 2019</span>
             </p>
             <ul>
               <li>Implementing a customizable reporting engine from front-to-back.
@@ -47,6 +51,10 @@ function Resume() {
               <span className="title">Full Stack Web Developer</span>
               <span className="time-period">Sep 2014 – Jul 2017</span>
             </p>
+            <p>
+              <span className="title">Digital Intern</span>
+              <span className="time-period">May 2014 – Sep 2014</span>
+            </p>
             <ul>
               <li>Developed 10 Ruby on Rails microsites for 6 national brands.</li>
               <li>Implemented VBI and content changes in Adobe Experience Manager for 5 national brands.</li>
@@ -54,25 +62,19 @@ function Resume() {
               <li>Set up Amazon Web Services – Elastic Beanstalk to handle 3 WordPress sites.</li>
               <li>Collaborated with 3rd party vendors to create and update content in 3 iOS applications.</li>
               <li>User Journey creation, Wire-framing, and UX design for corporate social responsibility site.</li>
-            </ul>
-            <p>
-              <span className="title">Digital Intern</span>
-              <span className="time-period">May 2014 – Sep 2014</span>
-            </p>
-            <ul>
-              <li>Collaborated with multi-disciplinary team to pitch rebranding campaign based on new strategic direction for national account and created digital concept designs.</li>
-              <li>Contributed to discussion about content-centered design best practices.</li>
-              <li>Responded to clients’ requests regarding updates in timely and efficient manner.</li>
-              <li>Improved user experience for branded digital applications and websites.</li>
-              <li>Applied troubleshooting processes to fix existing code and content on brand web pages.</li>
+              <li className="hidden">Collaborated with multi-disciplinary team to pitch rebranding campaign based on new strategic direction for national account and created digital concept designs.</li>
+              <li className="hidden">Contributed to discussion about content-centered design best practices.</li>
+              <li className="hidden">Responded to clients’ requests regarding updates in timely and efficient manner.</li>
+              <li className="hidden">Improved user experience for branded digital applications and websites.</li>
+              <li className="hidden">Applied troubleshooting processes to fix existing code and content on brand web pages.</li>
             </ul>
           </article>
         </section>
-        <section className="leadership-experience">
-          <h3>Leadership Experience</h3>
+        <section className="leadership">
+          <h2>Leadership Experience</h2>
           <article className="experience">
             <p>
-              <span className="organization">Alpha Phi Omega - <em>Alpha Phi Chapter</em></span>
+              <span className="organization">Alpha Phi Omega<em> - Alpha Phi Chapter</em></span>
               <span className="time-period">Jan 2011 – May 2014</span>
             </p>
             <p>
@@ -86,7 +88,7 @@ function Resume() {
               <li>Rebuilt and updated a Google spreadsheet for recording and tracking requirements.</li>
             </ul>
           </article>
-          <article className="experience">
+          <article className="experience hidden">
             <p>
               <span className="organization">Washington University Pops Orchestra</span>
               <span className="time-period">Sep 2010 – May 2014</span>
