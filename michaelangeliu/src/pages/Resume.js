6e03d0533c824f9f342f8bc5b22aa0db461edcbd
Michael Liu <michaelangeliu+git@gmail.com>
2020-02-02 12:25:31 -0600

    Modify Resume page
    - Use clearer language for summary section
    - Reorder SherpaCRM section and include additional detail regarding
    data pipeline
    - Include Group360 in We Are Alexander name
    - Change VBI to brand identity
    - Hide GPA information because out of school for several years now
    - Reorder Technical skills and include Tableau
    - Change columns for educationi/skills to be 50% without GPA
    - Decrease margins on print page

--- michaelangeliu/src/pages/Resume.js
+++ michaelangeliu/src/pages/Resume.js
@@ -9,9 +9,13 @@ function Resume() {
       </header>
       <main>
         <section className="summary">
-          <summary>Detail-oriented Software Developer with a 5+ years experience designing and implementing holistic solutions effectively and efficiently. Developed web applications for multi-national brands. Prototyped new features for an online CRM tool. Skilled in creating new documentation to improve long-term code maintainability, development process, and product stewardship.</summary>
+          <summary>
+            Detail-oriented Software Developer with 5+ years experience.
+            Passionate about designing and implementing holistic solutions effectively and efficiently.
+            Developed web applications for national brands.
+            Currently prototyping new features for an online CRM tool and improving long-term code maintainability with better development process, new documentation and clearer product stewardship.
+          </summary>
         </section>
-        ~
         <section className="work">
           <h2>Work Experience</h2>
           <article className="experience">
@@ -28,6 +32,10 @@ function Resume() {
               <span className="time-period">Aug 2017 – Aug 2019</span>
             </p>
             <ul>
+              <li>Rearchitecting front-end data flow in React and Redux to normalize state and reduce bundle size.</li>
+              <li>Redesigning transactional database occupancy tracking models to improve data accuracy within the application.</li>
+              <li>Collaborating with vendors to build a data warehouse ETL pipeline and Tableau embed.</li>
+              <li>Maintaining, troubleshooting, and optimizing the data warehouse pipeline and OLAP data models</li>
               <li>Implementing a customizable reporting engine from front-to-back.
                 <ul>
                   <li>React/Redux to display the data in tables and visualizations.</li>
@@ -35,8 +43,6 @@ function Resume() {
                   <li>MySQL/Redshift to query the database.</li>
                 </ul>
               </li>
-              <li>Collaborating with vendors to build a data warehouse ETL pipeline and Tableau embed.</li>
-              <li>Redesigning data models to improve occupancy tracking within the application.</li>
               <li>Standardizing and documenting team coding styles and best practices.</li>
               <li>Defining terminology and identify workflow loopholes with the product team.</li>
               <li>Mentoring junior developers in code optimization and GIT versioning.</li>
@@ -44,7 +50,7 @@ function Resume() {
           </article>
           <article className="experience">
             <p>
-              <span className="organization">We Are Alexander</span>
+              <span className="organization">We Are Alexander <em>(Formerly Group360 Worldwide)</em></span>
               <span className="location">Saint Louis, MO</span>
             </p>
             <p>
@@ -57,9 +63,9 @@ function Resume() {
             </p>
             <ul>
               <li>Developed 10 Ruby on Rails microsites for 6 national brands.</li>
-              <li>Implemented VBI and content changes in Adobe Experience Manager for 5 national brands.</li>
               <li>Feature development for Ruby on Rails experiential event tracking application on Heroku.</li>
               <li>Set up Amazon Web Services – Elastic Beanstalk to handle 3 WordPress sites.</li>
+              <li>Implemented brand identity and content changes in Adobe Experience Manager for 5 national brands.</li>
               <li>Collaborated with 3rd party vendors to create and update content in 3 iOS applications.</li>
               <li>User Journey creation, Wire-framing, and UX design for corporate social responsibility site.</li>
               <li className="hidden">Collaborated with multi-disciplinary team to pitch rebranding campaign based on new strategic direction for national account and created digital concept designs.</li>
@@ -91,7 +97,7 @@ function Resume() {
           <h2>Leadership Experience</h2>
           <article className="experience">
             <p>
-              <span className="organization">Alpha Phi Omega<em> - Alpha Phi Chapter</em></span>
+              <span className="organization">Alpha Phi Omega <em>- Alpha Phi Chapter</em></span>
               <span className="time-period">Jan 2011 – May 2014</span>
             </p>
             <p>
@@ -131,10 +137,10 @@ function Resume() {
                   <p className="degree-focus">B.S. in Computer Science, 2014</p>
                   <p className="degree-focus">Minor in Psychology</p>
                 </div>
-                <div className="right">
+                <div className="right hidden">
                   <p className="degree-focus">Cumulative GPA: 3.48/4.00</p>
                   <p className="degree-focus">Major GPA: 3.78/4.00</p>
-                  <p className="hidden">Dean’s List – Fall 2012, Fall 2013, and Spring 2014</p>
+                  <p className="">Dean’s List – Fall 2012, Fall 2013, and Spring 2014</p>
                 </div>
               </article>
             </section>
@@ -162,22 +168,24 @@ function Resume() {
               <article className="experience">
                 <ul className="comma-list">
                   <li>Javascript</li>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>GIT</li>
-                  <li>SQL</li>
-                  <li>Amazon Web Services</li>
-                  <li>Python</li>
-                  <li>Ruby</li>
                   <li className="hidden">React</li>
                   <li className="hidden">Redux</li>
                   <li className="hidden">Node</li>
-                  <li className="hidden">MySQL</li>
-                  <li className="hidden">PostgreSQL</li>
-                  <li className="hidden">Bootstrap</li>
                   <li className="hidden">jQuery</li>
                   <li className="hidden">Angular</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li className="hidden">Bootstrap</li>
+                  <li>GIT</li>
+                  <li>SQL</li>
+                  <li className="hidden">MySQL</li>
+                  <li className="hidden">PostgreSQL</li>
+                  <li>Tableau</li>
+                  <li>Amazon Web Services</li>
+                  <li>Python</li>
+                  <li>Data Warehousing</li>
                   <li className="hidden">Java</li>
+                  <li>Ruby</li>
                 </ul>
               </article>
             </section>
